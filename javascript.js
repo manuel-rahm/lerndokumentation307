60228ddc10b1b509af1dbd5aa212c69e3b89774a
Rahm Manuel <manuel.rahm@blnet.lokal>
2019-03-26 14:23:25 +0100

    some changes

--- javascript.js
+++ javascript.js
@@ -3,13 +3,13 @@ var hourNow = today.getHours();
 var greeting;
 
 if (hourNow > 18) {
-    greeting = 'Guten Abend!';
+    greeting = 'Guten Abend! Schauen Sie sich um!';
 } else if (hourNow > 12) {
-    greeting = 'Guten Tag!';
+    greeting = 'Guten Tag! Schauen Sie sich um!';
 } else if (hourNow > 0) {
-    greeting = 'Guten Morgen';
+    greeting = 'Guten Morgen! Schauen Sie sich um!';
 } else {
-    greeting = 'Willkommen';
+    greeting = 'Willkommen! Schauen Sie sich um!';
 }
 
 document.write('<h1 class="gruss">' + greeting + '</h1>')
