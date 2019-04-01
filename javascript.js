ca8bbfa6a02d711127b4714403c1dea81c8a89a9
Rahm Manuel <manuel.rahm@blnet.lokal>
2019-04-01 13:31:03 +0200

    some changes

--- javascript.js
+++ javascript.js
@@ -1,15 +1,18 @@
 var today = new Date();
-var hourNow = today.getHours();
-var greeting;
-
-if (hourNow > 18) {
-    greeting = 'Guten Abend! Schauen Sie sich um!';
-} else if (hourNow > 12) {
-    greeting = 'Guten Tag! Schauen Sie sich um!';
-} else if (hourNow > 0) {
-    greeting = 'Guten Morgen! Schauen Sie sich um!';
-} else {
-    greeting = 'Willkommen! Schauen Sie sich um!';
-}
-
-document.write('<h1 class="gruss">' + greeting + '</h1>');
+        var hourNow = today.getHours();
+        var greeting;
+        
+        if (hourNow > 18) {
+            greeting = 'Guten Abend! Schauen Sie sich um!';
+        } else if (hourNow > 12) {
+            greeting = 'Guten Tag! Schauen Sie sich um!';
+        } else if (hourNow > 0) {
+            greeting = 'Guten Morgen! Schauen Sie sich um!';
+        } else {
+            greeting = 'Willkommen! Schauen Sie sich um!';
+        }
+        
+        document.write('<h1 class="gruss hide">' + greeting + '</h1>');
+        
+        
+        $(".gruss").fadeIn(3000);
